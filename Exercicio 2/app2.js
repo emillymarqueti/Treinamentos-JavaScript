a057eb01a971a00919f43f43925cbe9f46e75901
Emilly Marqueti <emillymarqueti07@gmail.com>
2026-08-19 14:55:14 -0300

    Create app2.js

---
+++ Exercicio 2/app2.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var carro_1 = require("./carro");
+var meuCarro = new carro_1.Carro('Preto');
+meuCarro.acelerar();
+meuCarro.acelerar(15);
